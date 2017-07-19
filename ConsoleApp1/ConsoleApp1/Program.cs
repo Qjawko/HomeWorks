fd1ed197ae5601197cb62a061c1d8ac5a272d455
Qjawko <Qjawko@gmail.com>
2017-07-19 18:57:12 +0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ермагамбет Темирлан");
+
+            Console.WriteLine("Введите первое число");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Сумма равна = ");
+            Console.WriteLine(a+b);
+
+            Console.ReadLine();
+        }
+    }
+}
